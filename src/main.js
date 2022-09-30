7fbc2ec2fe7756573fee97de5a2791894ee5e314
btqf <2452501500@qq.com>
2022-09-30 16:01:43 +0800

    add doubleClick directive

--- src/main.js
+++ src/main.js
@@ -5,15 +5,16 @@ import stores from '@/store'
 import routers from '@/router'
 import '@/styles/style.scss'
 import win11UI from '@/plugins'
+import initDirectives from '@/utils/directive'
 import VueLazyload from 'vue-lazyload'
 
 
-
-const app = createApp(App)
+const app = createApp(App);
 app.use(VueLazyload, {
     lazyComponent: true
-})
-app.use(stores)
-app.use(routers)
-app.use(win11UI)
-app.mount('#app')
+});
+app.use(stores);
+app.use(routers);
+app.use(win11UI);
+initDirectives(app);
+app.mount('#app');
